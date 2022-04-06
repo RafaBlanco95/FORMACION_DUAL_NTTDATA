36af5d59054bc0d8601df642179d7aafa2b16e32
RafaBlanco95 <rafa.blanco.95@gmail.com>
2022-04-06 17:34:57 +0200

    Fifth Commit: branchTest hello

--- src/nttdatagitworkshop/HelloWorld.java
+++ src/nttdatagitworkshop/HelloWorld.java
@@ -15,8 +15,20 @@ public class HelloWorld {
 	 * Método principal de la clase
 	 */
 	public static void main(String[] args) {
-				
-		System.out.println("Buenas Téllez, te saludo desde la rama \"branchTest\"");
+		
+		// System.out.println("Hola mundo");
+		
+	    // System.out.println("Buenas Téllez, te saludo desde mi tercer commit");
+		
+		System.out.println("Saludos Cordiales Téllez, los commits realizados hasta ahora son los siguientes: \n");
+		
+		System.out.println("First Commit --> Se subieron las carpetas del proyecto al repositorio local ");
+		System.out.println("Second Commit --> Se añadió al .java los comentarios del javadoc ");
+		System.out.println("Third Commit --> Se ha incluido un segundo saludo");
+		System.out.println("Fourth Commit --> Se muestra esta lista de commits");
+		System.out.println("Fifth Commit --> Saludo desde rama \"branchTest\"\n");
+		
+		System.out.println("Buenas tardes Téllez, este saludo se implementó en la rama \"branchTest\"");
 		
 		
 		
